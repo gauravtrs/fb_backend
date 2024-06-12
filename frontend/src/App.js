b5ef406115d332da5208ac0d5427b6764ddadbc2
gauravaps <gauravapsout@look.com>
2024-06-12 14:20:19 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App
+      <img width="48" height="48" src="https://img.icons8.com/fluency/48/facebook-new.png" alt="facebook-new"/>
+    </div>
+  )
+}
+
+export default App
