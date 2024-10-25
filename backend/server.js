6fcd973d6017800a8e1b12675ac01b218b1f7504
gauravaps <gauravapsout@look.com>
2024-10-25 21:07:39 +0530

    fix the path

--- backend/server.js
+++ backend/server.js
@@ -45,13 +45,13 @@ const port = process.env.PORT || 5000;
 //for production route
 if(process.env.NODE_ENV === 'production'){
   //set static folder
-  app.use(express.static(path.join(__dirname,'/frontend/build')));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   //any route that is not will be redirected to index.html
 
   app.get('*' ,(req ,res) =>
-  res.sendFile(path.resolve(__dirname ,'frontend' ,'build' ,'index.html'))
-  );
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html")));
+
 }else{
   app.get('/' ,(req,res) =>{
       res.send('API is running...');
